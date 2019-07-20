d0c4082c71bf34ab262435d898eaa9cbb27e2f15
Michael Shtutman <mshtutman@shutterfly.com>
2019-07-21 00:12:29 +0300

    Cleanup.

--- src/components/SearchResults.jsx
+++ src/components/SearchResults.jsx
@@ -36,7 +36,7 @@ class SearchResults extends Component {
   renderMoreItems = hiddenItemsCount => {
     console.log(hiddenItemsCount);
     if (hiddenItemsCount > 0) {
-      return <p className="more-results-available">{hiddenItemsCount} more results are hidden</p>;
+      return <p className="more-results-available">{hiddenItemsCount} more results</p>;
     } else {
       return null;
     }
